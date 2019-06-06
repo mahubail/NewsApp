200a8243d88da8cc5e48222ecadcd7ba2c5756f8
mahubail <7ubail.ma@gmail.com>
2019-06-06 17:51:28 +0300

    add country and category

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,68 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 newsData:any;
+country;
+category;
+
+countryList = [
+{"name":"Any","val":"any"},
+{"name":"Argentina","val":"ar"},
+{"name":"Australia","val":"au"},
+{"name":"Austria","val":"at"},
+{"name":"Belgium","val":"be"},
+{"name":"Brazil","val":"br"},
+{"name":"Bulgaria","val":"bg"},
+{"name":"Canada","val":"ca"},
+{"name":"China","val":"cn"},
+{"name":"Colombia","val":"co"},
+{"name":"Cuba","val":"cu"},
+{"name":"Czech Republic","val":"cz"},
+{"name":"Egypt","val":"eg"},
+{"name":"France","val":"fr"},
+{"name":"Germany","val":"de"},
+{"name":"Greece","val":"gr"},
+{"name":"Hong Kong","val":"hk"},
+{"name":"Hungary","val":"hu"},
+{"name":"India","val":"in"},
+{"name":"Indonesia","val":"id"},
+{"name":"Ireland","val":"ie"},
+{"name":"Italy","val":"it"},
+{"name":"Japan","val":"jp"},
+{"name":"Latvia","val":"lv"},
+{"name":"Lithuania","val":"lt"},
+{"name":"Malaysia","val":"my"},
+{"name":"Mexico","val":"mx"},
+{"name":"Morocco","val":"ma"},
+{"name":"Netherlands","val":"nl"},
+{"name":"New Zealand","val":"nz"},
+{"name":"Nigeria","val":"ng"},
+{"name":"Norway","val":"no"},
+{"name":"Philippines","val":"ph"},
+{"name":"Poland","val":"pl"},
+{"name":"Portugal","val":"pt"},
+{"name":"Romania","val":"ro"},
+{"name":"Russia","val":"ru"},
+{"name":"Saudi Arabia","val":"sa"},
+{"name":"Serbia","val":"rs"},
+{"name":"Singapore","val":"sg"},
+{"name":"Slovakia","val":"sk"},
+{"name":"Slovenia","val":"si"},
+{"name":"South Africa","val":"za"},
+{"name":"South Korea","val":"kr"},
+{"name":"Sweden","val":"se"},
+{"name":"Switzerland","val":"ch"},
+{"name":"Taiwan","val":"tw"},
+{"name":"Thailand","val":"th"},
+{"name":"Turkey","val":"tr"},
+{"name":"UAE","val":"ae"},
+{"name":"Ukraine","val":"ua"},
+{"name":"United Kingdom","val":"gb"},
+{"name":"United States","val":"us"},
+{"name":"Venuzuela","val":"ve"}
+];
+
+categoryList;
+
   constructor(public navCtrl: NavController, private newsProvider:NewsProvider) {
 
     this.newsProvider.getNews("topHeadlines","country=us&category=general").subscribe(
