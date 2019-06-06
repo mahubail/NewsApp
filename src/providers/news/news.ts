121b1fd2a92979f54ef0d2ec242f26b495137d30
mahubail <7ubail.ma@gmail.com>
2019-06-06 16:42:31 +0300

    add getNews function

--- src/providers/news/news.ts
+++ src/providers/news/news.ts
@@ -9,9 +9,27 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class NewsProvider {
-
+  topHeadlinesUrl="https://newsapi.org/v2/top-headlines?";
+  //apiUrl="https://newsapi.org/v2/everything?country=us&apiKey=";
+  apiKey="&apiKey=8611252c534648fb8ce7be2cc3472f78";
   constructor(public http: HttpClient) {
     console.log('Hello NewsProvider Provider');
+  }
+  getNews(newsType, params)
+  {
+    console.log("getNews param:" + newsType);
+    console.log("getNews param:" + params);
+    if(newsType=="topHeadlines")
+    {
+      console.log(this.topHeadlinesUrl+ params + this.apiKey);
+      //return "";
+      return this.http.get(this.topHeadlinesUrl+ params + this.apiKey);
+    }
+    else
+    {
+
+    }
+
   }
 
 }
